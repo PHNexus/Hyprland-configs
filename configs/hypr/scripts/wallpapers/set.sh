03d5b6ca8cfdb438c7fe925fccd15443dc619e8a
PHNexus <marcostrup10@gmail.com>
2026-08-15 17:37:03 -0300

    configs

---
+++ configs/hypr/scripts/wallpapers/set.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eu
+
+WALL=$1
+
+awww img --transition-type center --transition-step 90 "$WALL"
+echo "Wallpaper set successfully"
+
+
+if command -v wal >/dev/null 2>&1; then
+    echo "applying pywal colors..."
+    wal -i "$WALL"
+    echo "pywal applied successfully"
+    "$HOME/.config/keyboard/set-color-keyboard.sh" &
+else
+    echo "pywal not installed, skipping"
+fi
+
