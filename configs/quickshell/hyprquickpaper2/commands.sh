ea0eddddc750ae97cc83435a473e8f1133342cdb
PHNexus <marcostrup10@gmail.com>
2026-08-26 18:28:02 -0300

    Sync: 2026-08-26 18:28

--- configs/quickshell/hyprquickpaper2/commands.sh
+++ configs/quickshell/hyprquickpaper2/commands.sh
@@ -5,11 +5,11 @@ WALL="$1"
 
 MONITORS=$(hyprctl monitors -j | jq -r '.[].name')
 
-CHOSEN_MONITOR=$(echo -e "All\n$MONITORS" | wofi --dmenu --prompt "Selecione o monitor")
+CHOSEN_MONITOR=$(echo -e "Todos\n$MONITORS" | wofi --dmenu --prompt "Selecione o monitor")
 
 [ -z "$CHOSEN_MONITOR" ] && exit 0
 
-if [ "$CHOSEN_MONITOR" = "All" ]; then
+if [ "$CHOSEN_MONITOR" = "Todos" ]; then
     for MONITOR in $MONITORS; do
         awww img -o "$MONITOR" "$WALL" -t random --transition-duration 1
     done
