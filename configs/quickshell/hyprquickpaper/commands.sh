6fa7c71633c93506c3aa8d4db2c6bee7ea292e18
PHNexus <marcostrup10@gmail.com>
2026-08-26 14:42:17 -0300

    Sync: 2026-08-26 14:42

--- configs/quickshell/hyprquickpaper/commands.sh
+++ configs/quickshell/hyprquickpaper/commands.sh
@@ -3,18 +3,16 @@ set -eu
 
 WALL="$1"
 
-# 1. Obter a lista de monitores conectados via hyprctl
 MONITORS=$(hyprctl monitors -j | jq -r '.[].name')
 
-# 2. Pergunta via Wofi em qual monitor deve ser aplicado
 CHOSEN_MONITOR=$(echo -e "Todos\n$MONITORS" | wofi --dmenu --prompt "Selecione o monitor")
 
-# Se cancelar o menu do Wofi, interrompe a alteração
 [ -z "$CHOSEN_MONITOR" ] && exit 0
 
-# 3. Executa o awww com base na escolha
 if [ "$CHOSEN_MONITOR" = "Todos" ]; then
-    awww img "$WALL" -t random --transition-duration 1[cite: 24]
+    for MONITOR in $MONITORS; do
+        awww img -o "$MONITOR" "$WALL" -t random --transition-duration 1
+    done
 else
     awww img -o "$CHOSEN_MONITOR" "$WALL" -t random --transition-duration 1
 fi
