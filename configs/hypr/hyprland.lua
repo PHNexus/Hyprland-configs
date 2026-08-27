9f4eac4218effe662eab1cc9c67dc6871c31ea7e
PHNexus <marcostrup10@gmail.com>
2026-08-26 22:45:13 -0300

    Sync: 2026-08-26 22:45

--- configs/hypr/hyprland.lua
+++ configs/hypr/hyprland.lua
@@ -218,10 +218,14 @@ hl.bind(mainMod .. " + V", hl.dsp.exec_cmd("cliphist list | wofi --dmenu | cliph
 hl.bind(mainMod .. " + L", hl.dsp.exec_cmd("hyprlock"))
 hl.bind(mainMod .. " + SHIFT + R", hl.dsp.exec_cmd(os.getenv("HOME") .. "/.config/hypr/scripts/wallpapers/set-random.sh"))
 hl.bind("ALT + TAB", hl.dsp.exec_cmd("qs ipc -c overview call overview toggle"))
+
 --hl.bind(mainMod .. " + W", hl.dsp.exec_cmd(os.getenv("HOME") .. "/.config/hypr/scripts/wallpapers/set-wallpaper.sh"))
 hl.bind(mainMod .. " + W",
     hl.dsp.exec_cmd("pgrep -x quickshell >/dev/null && pkill -x quickshell || quickshell -c hyprquickpaper"))
-hl.bind("SUPER + SHIFT + code:201", hl.dsp.exec_cmd("firefox https://claude.ai"))
+
+
+
+    hl.bind("SUPER + SHIFT + code:201", hl.dsp.exec_cmd("firefox https://claude.ai"))
 
 -- Toggle waybar
 hl.bind(mainMod .. " + SHIFT + W",
@@ -302,7 +306,7 @@ hl.on("hyprland.start", function()
     hl.exec_cmd("sleep 1 && waybar")
     hl.exec_cmd("hypridle")
     os.execute("nvibrant 0 512 512 0 >/dev/null 2>&1 &")
-    hl.exec_cmd("quickshell -c overview")
+  hl.exec_cmd("quickshell")
     hl.exec_cmd(
         "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP && gnome-keyring-daemon --start --components=secrets")
     hl.exec_cmd("awww-daemon")
---
+++ configs/quickshell/shell.qml
@@ -0,0 +1,13 @@
+import Quickshell
+
+Shell {
+    // Carrega ambos os painéis
+    QuickshellPanel {
+        id: overview
+        name: "overview"
+    }
+    QuickshellPanel {
+        id: hyprquickpaper
+        name: "hyprquickpaper"
+    }
+}
