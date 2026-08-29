3ce3f052460e8d8aadefbaacb87c6d7469fdccc2
PHNexus <marcostrup10@gmail.com>
2026-08-29 20:17:08 -0300

    Sync: 2026-08-29 20:17

--- configs/hypr/hyprland.lua
+++ configs/hypr/hyprland.lua
@@ -5,7 +5,7 @@ hl.monitor({ output = "DP-1", mode = "1920x1080@180", position = "0x0", scale = 
 hl.monitor({ output = "HDMI-A-1", mode = "1366x768@60", position = "-1366x0", scale = 1 })
 -- WORKSPACE
 -- DP-1 (primary monitor)
-hl.workspace_rule({ workspace = 1, monitor = "DP-1", persistent = true , default = true})
+hl.workspace_rule({ workspace = 1, monitor = "DP-1", persistent = true , default = true })
 hl.workspace_rule({ workspace = 2, monitor = "DP-1", persistent = true })
 hl.workspace_rule({ workspace = 3, monitor = "DP-1",  persistent = true })
 hl.workspace_rule({ workspace = 4, monitor = "DP-1", persistent = true })
