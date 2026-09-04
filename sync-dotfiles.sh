c198c1226e7877d24ace6d0431312efd94af2332
PHNexus <marcostrup10@gmail.com>
2026-09-04 10:50:17 -0300

    Sync: 2026-09-04 10:50

--- sync-dotfiles.sh
+++ sync-dotfiles.sh
@@ -6,7 +6,7 @@ CONFIG_DIR="$REPO_DIR/configs"
 
 CONFIG_FOLDERS=(
     "btop" "nvim" "cava" "fastfetch" "fish"
-    "hypr" "kitty" "quickshell" "swaync" "waybar" "wlogout" "wofi"
+    "hypr" "kitty" "quickshell" "swaync" "waybar" "gtk-3.0" "gtk-4.0" "wlogout" "wofi"
     "xdg-desktop-portal"
 )
 
