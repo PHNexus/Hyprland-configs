7bdecb729ae8569387e6f27749a6f91134d657f5
PHNexus <marcostrup10@gmail.com>
2026-09-04 04:09:08 -0300

    Sync: 2026-09-04 04:09

--- configs/hypr/hyprland.lua
+++ configs/hypr/hyprland.lua
@@ -26,6 +26,7 @@ hl.env("XDG_DATA_DIRS",
 -- NVIDIA
 hl.env("LIBVA_DRIVER_NAME", "nvidia")
 hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
+hl.env("GBM_BACKEND", "nvidia-drm")
 -- Wayland
 hl.env("OZONE_PLATFORM", "wayland")
 hl.env("ELECTRON_OZONE_PLATFORM_HINT", "wayland")
