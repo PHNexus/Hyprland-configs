6bc51b788f01697950e461c0b5a14d08e433794e
PHNexus <marcostrup10@gmail.com>
2026-09-06 11:13:05 -0300

    Sync: 2026-09-06 11:13

--- configs/fastfetch/storage.sh
+++ configs/fastfetch/storage.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 case "$1" in
   nvme)
-    findmnt -n -o SOURCE / | grep -E '^/dev/nvme' | xargs -I {} df -h {} 2>/dev/null | awk 'NR==2 {print $3 "/" $2}'
+    df -h / 2>/dev/null | awk 'NR==2 {print $3 "/" $2}'
     ;;
   ssd)
-    lsblk /dev/sda -b -o SIZE -nr 2>/dev/null | awk 'NR==1 {print "0/" int($1/1024/1024/1024) "G"}'
+    target=$(lsblk -o MOUNTPOINT,NAME,TYPE -rn | grep -E 'part|disk' | awk '$1 != "/" && $1 != "" {print $1; exit}')
+    if [ -n "$target" ]; then
+      df -h "$target" 2>/dev/null | awk 'NR==2 {print $3 "/" $2}'
+    fi
+    ;;
+  hd)
+    target=$(lsblk -o MOUNTPOINT,NAME,TYPE -rn | grep -E 'part|disk' | awk '$1 != "/" && $1 != "" {print $1}' | sed -n '2p')
+    if [ -n "$target" ]; then
+      df -h "$target" 2>/dev/null | awk 'NR==2 {print $3 "/" $2}'
+    fi
     ;;
 esac
