fedd1201ec74856d4837eec2096e7d40b0cc9b7c
PHNexus <marcostrup10@gmail.com>
2026-09-06 04:29:50 -0300

    Sync: 2026-09-06 04:29

--- configs/quickshell/hyprquickpaper/commands.sh
+++ configs/quickshell/hyprquickpaper/commands.sh
@@ -5,7 +5,7 @@ WALL="$1"
 
 MONITORS=$(hyprctl monitors -j | jq -r '.[].name')
 
-CHOSEN_MONITOR=$(echo -e "All\n$MONITORS" | wofi --dmenu --prompt "Selecione o monitor")
+CHOSEN_MONITOR=$(echo -e "All\n$MONITORS" | wofi --dmenu --prompt "Select your monitor")
 
 [ -z "$CHOSEN_MONITOR" ] && exit 0
 
