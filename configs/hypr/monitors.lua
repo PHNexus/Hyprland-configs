df83551bf288898db8c164b91923b488af0aecec
PHNexus <marcostrup10@gmail.com>
2026-09-13 13:50:55 -0300

    Sync: 2026-09-13 13:50

--- configs/hypr/monitors.lua
+++ configs/hypr/monitors.lua
@@ -6,7 +6,7 @@ function M.setup()
     hl.monitor({ output = "HDMI-A-1", mode = "1366x768@60", position = "-1366x0", scale = 1 })
 
     -- DP-1 (primary monitor)
-    hl.workspace_rule({ workspace = 1, monitor = "DP-1", persistent = true })
+    hl.workspace_rule({ workspace = 1, monitor = "DP-1", persistent = true, default = true })
     hl.workspace_rule({ workspace = 2, monitor = "DP-1", persistent = true })
     hl.workspace_rule({ workspace = 3, monitor = "DP-1", persistent = true })
     hl.workspace_rule({ workspace = 4, monitor = "DP-1", persistent = true })
