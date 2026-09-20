afdb4e24022edd50a082214b1a82e38620bf780d
PHNexus <marcostrup10@gmail.com>
2026-09-20 05:16:07 -0300

    Sync: 2026-09-20 05:16

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -26,7 +26,7 @@ set -gx CUDACXX /opt/cuda/bin/nvcc
 # Aliases
 # ============================================================
 alias minecraft "sklauncher; pkill -f sklauncher"
-alias sync-dotfiles "bash ~/Documents/GitHub/GitHub/Hyprland-configs/sync-dotfiles.sh"
+alias sync-dotfiles "bash ~/Documents/GitHub/Hyprland-configs/sync-dotfiles.sh"
 alias freeram "sync && sudo sysctl vm.drop_caches=3"
 alias xdg-open-appstream "gio open"
 
