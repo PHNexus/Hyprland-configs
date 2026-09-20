a35da1817f4aea169bdd2af2563bc74040bdce23
PHNexus <marcostrup10@gmail.com>
2026-09-20 04:23:58 -0300

    Sync: 2026-09-20 04:23

--- configs/hypr/general.lua
+++ configs/hypr/general.lua
@@ -28,10 +28,10 @@ function M.setup()
             column_width = 0.5,
             direction = "right",
             fullscreen_on_one_column = true,
-            focus_fit_method = 1, -- 0 Opens all windows centered; 1 opens windows side-by-side, maintaining scrolling.
-            explicit_column_widths = "0.333,0.5,0.667, 1.0",
+            focus_fit_method = 0, -- 0 Opens all windows centered; 1 opens windows side-by-side, maintaining scrolling.
+            explicit_column_widths = "0.5, 1.0",
             follow_focus = true,
-            follow_min_visible = 0.0,
+            follow_min_visible = 0.4,
         },
     })
 
